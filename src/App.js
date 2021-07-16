993871860b0e16cfb0cf6d7523d2f5553c00e242
Osahon Jonathan-Odia <emmyodia@gmail.com>
2021-07-16 07:05:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      id:'',
+      name:'',
+      answers:{
+        q1:'',
+        q2:'',
+        q3:'',
+        q4:''
+      }
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>survey app</h1>
+      </div>
+    );
+  }
+
 }
 
 export default App;
